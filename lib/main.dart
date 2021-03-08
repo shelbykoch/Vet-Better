87f95b5547214e99b6838963365eab2b0307335e
emchughes <emchughes@uco.edu>
2021-03-08 11:26:46 -0600

    merge with main

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:Capstone/Screen/forgot_password_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'Model/constant.dart';
+import 'Screen/login_screen.dart';
+import 'Screen/home_screen.dart';
+import 'Screen/factor_screen.dart';
+import 'Screen/personal_info_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: Constant.DEV,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        buttonColor: Color.fromRGBO(77, 225, 225, 90),
+        accentColor: Color.fromRGBO(77, 225, 225, 90),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      title: 'Login',
+      initialRoute: LoginScreen.routeName,
+      routes: {
+        LoginScreen.routeName: (context) => LoginScreen(),
+        HomeScreen.routeName: (context) => HomeScreen(),
+        ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
+        FactorScreen.routeName: (context) => FactorScreen(),
+        PersonalInfoScreen.routeName: (context) => PersonalInfoScreen(),
+      },
+    );
+  }
+}
